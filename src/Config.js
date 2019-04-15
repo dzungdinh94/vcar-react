47bf4b7570530d3ce90515c86def2ef51782fa54
toanprox7 <toanpro7x@gmail.com>
2019-04-15 10:12:29 +0700

    update api

--- src/Config.js
+++ src/Config.js
@@ -1,5 +1,5 @@
 import { createBrowserHistory } from 'history'
-let domain = 'http://157.230.33.218:3000/'
+let domain = 'http://139.162.50.22:3000/'
 // let domain = 'http://localhost:3000/'
 
 export const Config = {
