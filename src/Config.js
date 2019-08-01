62b3b7af47669148ac4717bda9ef40cff166fb0d
Toàn SajXuko <toanpro7x@gmail.com>
2019-08-01 01:33:01 +0000

    Update Config.js

--- src/Config.js
+++ src/Config.js
@@ -1,7 +1,7 @@
 import { createBrowserHistory } from 'history'
-// let domain = 'http://128.199.236.30:3000/'
+let domain = 'http://128.199.236.30:3001/'
 // let domain = 'http://localhost:3000/'
-let domain = 'http://139.162.50.22:3000/'
+// let domain = 'http://139.162.50.22:3000/'
 
 export const Config = {
     serviceUrl: domain + 'admin/',
