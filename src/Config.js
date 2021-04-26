148ed9cfe376bff2093de135fd64bc7e86bd511e
Dũng Nguyễn Đình <dungnd8594@gmail.com>
2021-04-27 03:00:42 +0700

    init

--- src/Config.js
+++ src/Config.js
@@ -1,5 +1,5 @@
 import { createBrowserHistory } from 'history'
-let domain = 'https://pacific-stream-38316.herokuapp.com/'
+let domain = 'https://vcar-backend.herokuapp.com/'
 // let domain = 'http://localhost:3000/'
 // let domain = 'http://139.162.50.22:3000/'
 
